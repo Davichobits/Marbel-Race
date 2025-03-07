590210217cdabc0cce68047cee31d1adc4590a9b
Davichobits <davidruizdeveloper@gmail.com>
2025-03-07 14:45:35 -0500

    Separate files from each geometry

--- sphere.js
+++ sphere.js
@@ -1,26 +1,26 @@
 import WebGL from 'three/addons/capabilities/WebGl.js';
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// DRAWING A SPHERE
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(1,64,32);
 const material = new THREE.MeshBasicMaterial({
   color:0x00f4,
   wireframe: true,
 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 camera.position.z = 5;
 
 function animate(){
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
@@ -29,5 +29,5 @@ if (WebGL.isWebGL2Available()){
   animate();
 }else{
   const warning = WebGL.getWebGL2ErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
+  document.getElementById( 'container' ).appendChild( warning );
 }
